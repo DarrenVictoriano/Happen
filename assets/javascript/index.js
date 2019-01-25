cea29816d7bc8da7f4404dff1f777f2f17374809
Darren Victoriano <darren.victoriano@gmail.com>
2019-01-24 20:57:45 -0800

    modal template

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,5 +1,9 @@
-var typed = new Typed("#typed-movement",{
-    strings:["changes", "decisions", "progress", "it"],
+var typed = new Typed("#typed-movement", {
+    strings: ["changes", "decisions", "progress", "it"],
     backSpeed: 50,
     typeSpeed: 50
+});
+
+$("#login").on("click", function () {
+    $("#login-modal").modal();
 });
