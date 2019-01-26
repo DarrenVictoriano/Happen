a36992eeb7c41ffcabff0a32525834e6a08dd867
Darren Victoriano <darren.victoriano@gmail.com>
2019-01-26 12:38:02 -0800

    added calendar

--- assets/javascript/today.js
+++ assets/javascript/today.js
@@ -1,5 +1,23 @@
 // Nav bar mobile activator
-$(document).ready(function() {
+$(document).ready(function () {
     $('.sidenav').sidenav();
+
+    $("#calendar-table").fullCalendar({
+        timeZone: 'UTC',
+        themeSystem: 'bootstrap4',
+        defaultView: 'agendaDay',
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay,listMonth'
+        },
+        bootstrapGlyphicons: {
+            prev: 'fa-chevron-left',
+            next: 'fa-chevron-right'
+        },
+        weekNumbers: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: 'https://fullcalendar.io/demo-events.json'
+    });
 });
 // Nav bar mobile end
