abf57f704f175cbfeafa15fe54c4983276c4e22f
notmsc <mscaro22@gmail.com>
2019-01-27 21:36:05 -0800

    Corrected spelling

--- assets/javascript/today.js
+++ assets/javascript/today.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     $('.sidenav').sidenav();
 
-    $("#calander-table").fullCalendar({
+    $("#calendar-table").fullCalendar({
         timeZone: 'UTC',
         themeSystem: 'bootstrap4',
         defaultView: 'agendaDay',
